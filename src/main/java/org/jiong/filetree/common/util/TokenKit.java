6406bd533c4b787820ae5e363ed95f10b9cf898e
ricky <anotherhe@foxmail.com>
2020-01-23 00:13:51 +0800

    modify: 实现token创建，增加测试用例

--- src/main/java/org/jiong/filetree/common/util/TokenKit.java
+++ src/main/java/org/jiong/filetree/common/util/TokenKit.java
@@ -1,17 +1,28 @@
 package org.jiong.filetree.common.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Base64;
 
 /**
  * @author Mr.Jiong
  */
 public class TokenKit {
+    private static DateTimeFormatter DETAIL_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS");
+
     public static String encode(String uniqueKey) {
         return Base64.getEncoder().encodeToString(uniqueKey.getBytes());
     }
 
+    /**
+     * create token use timestamp and a app flag
+     *
+     * @return a new unique token
+     */
     public static String newToken() {
-        // todo impl token creator
-        return null;
+        final String appFlag = "LFS";
+        String formattedTime = LocalDateTime.now().format(DETAIL_TIME_FORMATTER);
+        return appFlag + "_" + formattedTime;
     }
+
 }
