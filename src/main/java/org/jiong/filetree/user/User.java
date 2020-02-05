28b90b111db4886c4743aaa7f693a36f92dddb2e
ricky <anotherhe@foxmail.com>
2020-02-05 21:50:20 +0800

    modify: 实现更新用户token

--- src/main/java/org/jiong/filetree/user/User.java
+++ src/main/java/org/jiong/filetree/user/User.java
@@ -1,19 +1,27 @@
 package org.jiong.filetree.user;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.jiong.filetree.token.HandleToken;
 
 /**
  * @author Mr.Jiong
  */
 @Data
+@Slf4j
 public class User {
 
     private String name;
     private String infoPath;
     private HandleToken token;
 
+    /**
+     * Update user's status in session immediately,
+     * when user session invalidated, write user info to file
+     * @param token new token info
+     */
     public void updateToken(HandleToken token) {
-        // todo
+        log.info("Token to update: {}", token);
+        this.token = token;
     }
 }
