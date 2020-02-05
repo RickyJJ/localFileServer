ed3a634cafc46db74b2c4039f07520ecbbf06399
ricky <anotherhe@foxmail.com>
2020-02-05 21:53:04 +0800

    modify: 修改以兼容测试

--- src/main/java/org/jiong/filetree/AppBoot.java
+++ src/main/java/org/jiong/filetree/AppBoot.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 
 import javax.annotation.PostConstruct;
-import java.sql.SQLOutput;
 
 /***
  *
@@ -15,9 +15,18 @@ import java.sql.SQLOutput;
  */
 @SpringBootApplication
 public class AppBoot extends SpringBootServletInitializer {
+    private static ApplicationContext appContext;
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return super.configure(builder.sources(AppBoot.class));
+    }
+
+    public static ApplicationContext getAppContext() {
+        return appContext;
+    }
+
+    public static void setAppContext(ApplicationContext appContext) {
+        AppBoot.appContext = appContext;
     }
 
     @PostConstruct
@@ -31,6 +40,6 @@ public class AppBoot extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(AppBoot.class, args);
+        appContext = SpringApplication.run(AppBoot.class, args);
     }
 }
